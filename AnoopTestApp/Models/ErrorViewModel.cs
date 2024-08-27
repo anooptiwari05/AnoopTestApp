3a5305e43c2e129057c30fe7f6ade153628d62e2
anoop <anoop@RANGOLI-PC>
2024-08-27 14:32:17 -0400

    Add project files.

---
+++ AnoopTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnoopTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
